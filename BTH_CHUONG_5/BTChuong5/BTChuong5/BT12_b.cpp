8f714e83d39bc7a5365c9cc5413af33617d74d0f
kimman1 <41507148+kimman1@users.noreply.github.com>
2019-08-02 14:41:50 +0700

    Update BT12_b.cpp

--- BTH_CHUONG_5/BTChuong5/BTChuong5/BT12_b.cpp
+++ BTH_CHUONG_5/BTChuong5/BTChuong5/BT12_b.cpp
@@ -10,9 +10,16 @@ void main()
 		if(h<=0)
 			cout << "Nhap sai. Nhap lai " << endl;
 	}while(h<=0);
-	for(int i=1; i<=h ; i++)
-	{
-	
-	}
+	for(int i =h; i >= 1; i--)
+  {
+      for(int j = 1; j <= h; j++)
+      {
+       if(j<=i-1 )
+        cout << " ";
+        else
+        cout << "*";
+      }
+      cout << endl;
+  }
 	system("pause");
 }
